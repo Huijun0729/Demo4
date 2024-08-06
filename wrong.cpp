3a28521656528c1717de572df8d9c0fd1c486470
Huijun0729 <yaphuijun0729@e.newera.edu.my>
2024-08-06 17:52:06 +0800

    Update wrong.cpp

--- wrong.cpp
+++ wrong.cpp
@@ -59,6 +59,11 @@ int main() {
         cout << "Do you want to play again? (y/n): ";
         cin >> playAgain;
     } while (playAgain == 'y' || playAgain == 'Y');
+    
+    // Goodbye message when the user chooses not to play again
+    if (playAgain == 'n' || playAgain == 'N') {
+        cout << "Goodbye!" << endl;
+    }
 
     return 0;
 }
