367ff0cdd182f49d0f2b2468946145f8bebf4448
Huijun0729 <yaphuijun0729@e.newera.edu.my>
2024-08-06 23:12:32 +0800

    Update DM-4true.cpp

--- DM-4true.cpp
+++ DM-4true.cpp
@@ -66,5 +66,10 @@ int main() {
         cout << "Goodbye!" << endl;
     }
 
+     // Goodbye message when the user chooses not to play again
+    if (playAgain == 'n' || playAgain == 'N') {
+        cout << "Goodbye!" << endl;
+    }
+    
     return 0;
 }
